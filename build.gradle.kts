55f5b899765e2dd4e62504353d5610597314393a
Liad Nachlai <liadnachlai@gmail.com>
2025-03-31 09:36:06 +0300

    Added gradle versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
     alias(libs.plugins.kotlin.plugin.serialization)
+    alias(libs.plugins.gradle.versions.plugin)
 }
 
 group = "sh.deut.recipeapp"
