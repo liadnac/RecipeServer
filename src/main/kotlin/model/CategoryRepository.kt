9473caef79b63be719d44c9f9410554d696b8024
Liad Nachlai <liadnachlai@gmail.com>
2025-04-07 12:51:25 +0300

    Added new category post endpoint

--- src/main/kotlin/model/CategoryRepository.kt
+++ src/main/kotlin/model/CategoryRepository.kt
@@ -3,4 +3,5 @@ package sh.deut.recipeapp.model
 interface CategoryRepository {
     suspend fun allCategories(): List<Category>
     suspend fun categoryById(id: Int): Category?
+    suspend fun addCategory(category: Category)
 }
